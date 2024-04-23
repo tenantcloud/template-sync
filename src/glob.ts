75363078efaaaf834a938ff4d9514f812a5e21c1
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-04-23 18:31:45 +0300

    fix: Ignore .git directory

--- src/glob.ts
+++ src/glob.ts
@@ -2,12 +2,16 @@ import * as R from "remeda";
 import { globby } from "globby";
 
 export async function glob(
-	patterns: string | readonly string[],
+	patterns: string | string[],
 	root: string,
 	ignore: string[] = [],
 ): Promise<string[]> {
+	if (typeof patterns === "string") {
+		patterns = [patterns];
+	}
+
 	return R.difference.multiset(
-		await globby(patterns, { cwd: root, dot: true }),
+		await globby([...patterns, "!.git/**/*"], { cwd: root, dot: true }),
 		ignore,
 	);
 }
